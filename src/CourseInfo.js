367e496909bd7cb2c3bfae5b595b3ad21efe16b8
Emilie Rajka <enrajka@gmail.com>
2019-10-07 16:13:40 -0500

    Add files via upload

--- src/CourseInfo.js
+++ src/CourseInfo.js
@@ -9,11 +9,7 @@ class CourseInfo extends React.Component {
         super(props);
     }
    
-    sendInfo (props) {
-        console.log(this.props.data);
-    }
-
-    
+       
 
     getListG() {
         let listG = [];
@@ -21,21 +17,10 @@ class CourseInfo extends React.Component {
         if (this.props.data.sections != undefined) {
             let sec = Object.keys(this.props.data.sections);
             let secBody = Object.values(this.props.data.sections);
-             console.log(secBody);
-            // console.log(sec[0]);
-            // console.log(sec.length);           
-            // //listG.push(<ListGroup.Item>{sec[i]}</ListGroup.Item>);
             for (var i = 0; i < sec.length; i++) {
-                listG.push(<Card><Accordion.Toggle as={Card.Header} eventKey="0">{sec[i]}</Accordion.Toggle><Accordion.Collapse eventKey="0"><Card.Body>{this.getBody()}{secBody[i].instructor} : {secBody[i].location}</Card.Body></Accordion.Collapse></Card>);
+                listG.push(<Card><Accordion.Toggle as={Card.Header} eventKey={i}>{sec[i]}</Accordion.Toggle><Accordion.Collapse eventKey={i}><Card.Body>{this.getBody(secBody[i])}<Card.Text>{this.getTimes(secBody[i])}</Card.Text>{this.getTagS(secBody[i])}</Card.Body></Accordion.Collapse></Card>);
             }
             return listG;
-
-            // <Card>
-            //     <Accordion.Toggle as={Card.Header} eventKey="0">{sec[i]}</Accordion.Toggle>
-            //             <Accordion.Collapse eventKey="0">
-            //             <Card.Body>Hello! I'm the body</Card.Body>
-            //             </Accordion.Collapse>
-            // </Card>
         } 
         return "";
     }
@@ -44,6 +29,63 @@ class CourseInfo extends React.Component {
         if (this.props.data.sections != undefined) {
             return this.props.data.number + " : " + this.props.data.credits + " credits";
         } 
+        return "";
+    }
+
+    getBody (data) {
+        var s = data.instructor + " : " + data.location;
+        return s;
+    }
+
+    getTimes(data) {
+        let day = Object.keys(data.time);
+        let time = Object.values(data.time);
+        var s = "";
+        for (var i = 0; i < day.length; i++) {
+            s += (day[i].charAt(0).toUpperCase() + day[i].slice(1)) + "\t" + time[i] + "\n";
+        }
+        return s;
+    }
+
+    getSBody (data) {
+        console.log(data);
+        var s = data.location;
+        return s;
+    }
+
+    getTagS (data) {
+        let subs = Object.keys(data.subsections);
+        if (subs.length > 0) {
+            return <Accordion defaultActiveKey="0">{this.getSubSection(data)}</Accordion>;
+        }
+        return "";
+    }
+
+
+    getSubSection(data) {
+        if (data != undefined) {
+            let listS = [];
+            let subs = Object.keys(data.subsections);
+            let subBody = Object.values(data.subsections);
+            if (subs.length > 0) {
+                for (var i = 0; i < subs.length; i++) {
+                    listS.push(<Card><Accordion.Toggle as={Card.Header} eventKey={i}>{subs[i]}</Accordion.Toggle><Accordion.Collapse eventKey={i}><Card.Body>{subBody[i].location}<Card.Text>{this.getTimes(subBody[i])}</Card.Text></Card.Body></Accordion.Collapse></Card>);
+                }
+                return listS;
+            }
+        }
+        
+
+        // let listS = [];
+        // if (data.subsections != undefined) {
+        //     let sec = Object.keys(data.subsections);
+        //     let secBody = Object.values(data.subsections);
+        //     console.log(sec[0] + " "  + secBody[0]);
+        //     // for (var i = 0; i < sec.length; i++) {
+        //     //     listS.push(<Card><Accordion.Toggle as={Card.Header} eventKey={i}>{sec[i]}</Accordion.Toggle><Accordion.Collapse eventKey={i}><Card.Body>{this.secBody[i].location}<Card.Text>{this.getTimes(secBody[i])}</Card.Text></Card.Body></Accordion.Collapse></Card>);
+        //     // }
+        //     return listS;
+        // } 
         return "";
     }
 
