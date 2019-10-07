2b1e15f2dee25521ae631f2a89b4b509e8c6b0a2
Emilie Rajka <enrajka@gmail.com>
2019-10-07 14:36:06 -0500

    Update CourseInfo.js

--- src/CourseInfo.js
+++ src/CourseInfo.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Accordion from 'react-bootstrap/Accordion';
 
 class CourseInfo extends React.Component {
     constructor(props) {
@@ -12,32 +13,38 @@ class CourseInfo extends React.Component {
         console.log(this.props.data);
     }
 
-    getSubTitle () {
-        return this.props.data.number + "-" + this.props.data.credits + " credits";
-    }
+    
 
     getListG() {
         let listG = [];
         //console.log(this.props.data.sections);
         if (this.props.data.sections != undefined) {
             let sec = Object.keys(this.props.data.sections);
-            // console.log(sec);
+            let secBody = Object.values(this.props.data.sections);
+             console.log(secBody);
             // console.log(sec[0]);
             // console.log(sec.length);           
+            // //listG.push(<ListGroup.Item>{sec[i]}</ListGroup.Item>);
             for (var i = 0; i < sec.length; i++) {
-                 listG.push(<ListGroup.Item>{sec[i]}</ListGroup.Item>);
+                listG.push(<Card><Accordion.Toggle as={Card.Header} eventKey="0">{sec[i]}</Accordion.Toggle><Accordion.Collapse eventKey="0"><Card.Body>{this.getBody()}</Card.Body></Accordion.Collapse></Card>);
             }
             return listG;
+
+            // <Card>
+            //     <Accordion.Toggle as={Card.Header} eventKey="0">{sec[i]}</Accordion.Toggle>
+            //             <Accordion.Collapse eventKey="0">
+            //             <Card.Body>Hello! I'm the body</Card.Body>
+            //             </Accordion.Collapse>
+            // </Card>
         } 
         return "";
-        // if (this.props.data != undefined) {
-        //     let listG = {};
-        //     for (var i = 0; i < this.props.data.sections.length; i++) {
-        //         listG.push(<ListGroup.Item>{this.props.data.sections[i]}</ListGroup.Item>);
-        //     }
-        //     return listG;
-        // }
-        // return "";
+    }
+
+    getSubTitle () {
+        if (this.props.data.sections != undefined) {
+            return this.props.data.number + " : " + this.props.data.credits + " credits";
+        } 
+        return "";
     }
 
    render () {
@@ -45,11 +52,11 @@ class CourseInfo extends React.Component {
        return (
            <Card>
                 <Card.Title>{this.props.data.name}</Card.Title>
-                <Card.Subtitle>{this.props.data.number} : {this.props.data.credits} credits</Card.Subtitle>
+                <Card.Subtitle>{this.getSubTitle()}</Card.Subtitle>
                 <Card.Text>{this.props.data.description}</Card.Text>
-                <ListGroup variant="flush">
+                <Accordion defaultActiveKey="0">
                     {this.getListG()}
-                </ListGroup>
+                </Accordion>
            </Card>
             
        )
